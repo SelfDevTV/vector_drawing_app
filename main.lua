579bddc1bdf324d415b19180afff02c9deeecd21
SelfDevTV <selfdevtv@gmail.com>
2025-03-11 07:34:27 +0100

    first commit

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+    love.window.setTitle("Vector Drawing App")
+end
+
+function love.update(dt)
+    -- Update your logic here
+end
+
+function love.draw()
+    love.graphics.print("Welcome to Vector Drawing App!", 100, 100)
+end
